7daf8da287d00225c76ce0f28d5e7c8f0da3d201
Eugene Wekesa <eugenco578@gmail.com>
2025-04-10 12:16:26 +0300

    implement import/export

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,9 @@ import React from "react";
 function Home() {
   return (
     <div id="home">
-      <h1>
-        {username} is a Web Developer from {city}
-      </h1>
+      <h1>Home</h1>
     </div>
   );
 }
+
+export default Home;
